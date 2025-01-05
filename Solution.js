bbb9d0125116be48886552316f815842140c026e
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-01-05 11:02:48 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,31 @@
+
+/**
+ * @param {string} source
+ * @param {string} target
+ * @return {boolean}
+ */
+var isOneEditDistance = function (source, target) {
+    if (Math.abs(source.length - target.length) > 1) {
+        return false;
+    }
+
+    const shorterInput = (source.length <= target.length) ? source : target;
+    const longerInput = (source.length > target.length) ? source : target;
+
+    let indexShorter = 0;
+    let indexLonger = 0;
+    let countDifferentChars = 0;
+
+    while (indexShorter < shorterInput.length && indexLonger < longerInput.length && countDifferentChars < 2) {
+        if (shorterInput[indexShorter] === longerInput[indexLonger]) {
+            ++indexShorter;
+            ++indexLonger;
+            continue;
+        }
+        ++countDifferentChars;
+        indexShorter += 1 ^ (longerInput.length - shorterInput.length);
+        ++indexLonger;
+    }
+
+    return countDifferentChars + (longerInput.length - indexLonger) === 1;
+};
